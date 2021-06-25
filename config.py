90473d126615f0385e51fb21ea30c72b14beb8f7
veliebm <veliebm@gmail.com>
2021-06-25 11:18:09 -0400

    Adapt config to our pipeline

--- config.py
+++ config.py
@@ -19,32 +19,24 @@ user = getpass.getuser()  # Username of the user running the scripts
 host = getfqdn()  # Hostname of the machine running the scripts
 
 # You want to add your machine to this list
-if user == 'vanvliet':
-    # My laptop
-    raw_data_dir = './data'
-    n_jobs = 4  # My laptop has 4 cores
-elif host == 'nbe-024.org.aalto.fi' and user == 'vanvlm1':
-    # My workstation
-    raw_data_dir = './data'
-    n_jobs = 8  # My workstation has 8 cores
+if user == "csea":
+    # CSEA desktop
+    raw_data_dir = "./data"
+    n_jobs = 12  # The CSEA desktop has 12 logical processors
 else:
     # Defaults
-    raw_data_dir = './data'
+    raw_data_dir = "./data"
     n_jobs = 1
 
 # For BLAS to use the right amount of cores
-os.environ['OMP_NUM_THREADS'] = str(n_jobs)
+os.environ["OMP_NUM_THREADS"] = str(n_jobs)
 
 
 ###############################################################################
 # These are all the relevant parameters for the analysis.
 
-sample_rate = 1000  # Hz
-fmin = 1.0  # Hz
-fmax = 20.0  # Hz
-
-# All subjects
-subjects = ['sub01', 'sub02']
+# All subjects for whom our analysis can actually work.
+SUBJECTS = "104 106 107 108 109 110 111 112 113 115 116 117 120 121 122 123 124 125".split()
 
 ###############################################################################
 # Templates for filenames
@@ -54,24 +46,22 @@ subjects = ['sub01', 'sub02']
 # See fnames.py for details on how this class works.
 fname = FileNames()
 
-# Some directories
-fname.add('raw_data_dir', raw_data_dir)
-fname.add('processed_data_dir', './processed')
-fname.add('figures_dir', './figures')
+# Raw data directories.
+fname.add("raw_data_dir", raw_data_dir)
+fname.add("raw_subject_dir", "{raw_data_dir}/subjects-complete/sub-{subject}")
+fname.add("fmriprep_subject_dir", "{raw_data_dir}/fmriprep/sub-{subject}/fmriprep")
+fname.add("brainvision_dir", "{raw_data_dir}/brainvision/EXPORT")
+
+# Processed data directories.
+fname.add("processed_data_dir", "./processed")
+
+# Task: Create BIDS root.
+fname.add("bids_root", "{processed_data_dir}/bids")
+fname.add("bids_description", "{bids_root}/dataset_description.json")
 
 # The data files that are used and produced by the analysis steps
-fname.add('input', '{raw_data_dir}/input-{subject}.txt')
-fname.add('output', '{processed_data_dir}/output-{subject}.txt')
-fname.add('grand_average', '{processed_data_dir}/grand_average.txt')
-
-# The figures
-fname.add('figure1', '{figures_dir}/figure1.pdf')
-fname.add('figure_grand_average', '{figures_dir}/figure_grand_average.pdf')
-
-# Filenames for MNE reports
-fname.add('reports_dir', './reports/')
-fname.add('report', '{reports_dir}/{subject}-report.h5')
-fname.add('report_html', '{reports_dir}/{subject}-report.html')
+fname.add("output", "{processed_data_dir}/output-{subject}.txt")
+fname.add("grand_average", "{processed_data_dir}/grand_average.txt")
 
 # File produced by check_system.py
-fname.add('system_check', './system_check.txt')
+fname.add("system_check", "./system_check.txt")
