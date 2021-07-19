0a16f664eef171d3365e1c95fadfc63f1f7d5f77
veliebm <veliebm@gmail.com>
2021-07-19 11:29:02 -0400

    Ensure we mkdir before resampling

--- resample.py
+++ resample.py
@@ -8,11 +8,15 @@ veliebm@ufl.edu
 # Import external modules and libraries.
 from os import PathLike
 import subprocess
+from pathlib import Path
 
 def main(from_image: PathLike, to_image: PathLike, to_prefix: PathLike) -> None:
     """
     Aligns our IRFs to the MNI space used by the Kastner cortex mask.
     """
+    to_dir = Path(to_prefix).parent
+    to_dir.mkdir(exist_ok=True, parents=True)
+    
     command = f"""
         3dresample
         -master {to_image}
