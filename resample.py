dd9b1143531b36ba489940ebf159b4e30d22ebf3
veliebm <veliebm@gmail.com>
2021-08-04 11:58:09 -0400

    Add function to manually change resolution

--- resample.py
+++ resample.py
@@ -10,9 +10,9 @@ from os import PathLike
 import subprocess
 from pathlib import Path
 
-def main(from_image: PathLike, to_image: PathLike, to_prefix: PathLike) -> None:
+def main2(from_image: PathLike, to_image: PathLike, to_prefix: PathLike) -> None:
     """
-    Aligns our IRFs to the MNI space used by the Kastner cortex mask.
+    Resample an image to another image.
     """
     to_dir = Path(to_prefix).parent
     to_dir.mkdir(exist_ok=True, parents=True)
@@ -20,6 +20,22 @@ def main(from_image: PathLike, to_image: PathLike, to_prefix: PathLike) -> None:
     command = f"""
         3dresample
         -master {to_image}
+        -input {from_image}
+        -prefix {to_prefix}
+    """.split()
+    subprocess.run(command)
+
+
+def main2(from_image: PathLike, to_prefix: PathLike, dx: str, dy: str, dz: str) -> None:
+    """
+    Resample an image to a new resolution.
+    """
+    to_dir = Path(to_prefix).parent
+    to_dir.mkdir(exist_ok=True, parents=True)
+    
+    command = f"""
+        3dresample
+        -dxyz {dx} {dy} {dz}
         -input {from_image}
         -prefix {to_prefix}
     """.split()
