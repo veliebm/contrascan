06d59d20312771726ecd98fd498e51bbfbf74a21
veliebm <veliebm@gmail.com>
2021-09-23 08:36:15 -0400

    Correctl read amplitudes

--- correlate_regions.py
+++ correlate_regions.py
@@ -12,6 +12,7 @@ import pandas
 import argparse
 import matplotlib.pyplot as plt
 from pathlib import Path
+import scipy.io
 
 
 def main(load_microROI_from: PathLike, load_amplitudes_from: PathLike, save_scatter_to: PathLike, save_table_to: PathLike, save_spearman_to: PathLike) -> None:
@@ -20,7 +21,7 @@ def main(load_microROI_from: PathLike, load_amplitudes_from: PathLike, save_scat
     """
     Path(save_spearman_to).parent.mkdir(parents=True, exist_ok=True)
 
-    amplitudes = read_all_amplitudes(load_amplitudes_from)[20].tolist()
+    amplitudes = get_amplitudes(load_amplitudes_from).tolist()
     microROI = read_average(load_microROI_from)[:len(amplitudes)]
 
     table = make_table(
@@ -96,20 +97,89 @@ def read_average(path: PathLike) -> None:
     return averages
 
 
-def read_all_amplitudes(eeg_amplitudes_path: PathLike) -> pandas.DataFrame:
+def get_amplitudes(mat_path: PathLike) -> pandas.DataFrame:
     """
-    Returns all amplitudes from an EEG amplitudes file as a pretty dataframe.
-    """
-    all_amplitudes = pandas.read_table(
-        eeg_amplitudes_path,
-        sep="\t",
-        header=None,
-        index_col=False,
-    ).T
-    
-    all_amplitudes.columns += 1     # Make the channel numbers correspond to the MatLab channel numbers
+    Gets amplitudes from a mat file. Mat file must contain only an Nx1 vector.
 
-    return all_amplitudes
+    Parameters
+    ----------
+    mat_path : PathLike
+        Path to a .mat file.
+    
+    Returns
+    -------
+    pandas.Series
+        Series of numbers extracted from the mat file.
+    
+    Raises
+    ------
+    TypeError
+        When the mat file contains data that isn't an Nx1 vector.
+    """
+    mat = load_mat_file(mat_path)
+    
+    _check_array_is_vector(mat)
+
+    return pandas.Series(mat.reshape(-1))
+
+
+def _check_array_is_vector(array: numpy.array) -> None:
+    """
+    Raises TypeError if the array is not 1xN or Nx1.
+
+    Parameters
+    ----------
+    array : numpy.array
+        Should be 2 dimensional and be shaped either (1,N) or (N,1)
+
+    Raises
+    ------
+    TypeError
+        Array is not 2 dimensional or is not shaped correctly.
+    """
+    shape = array.shape
+    
+    try:
+        assert len(shape) == 2
+    except AssertionError:
+        raise TypeError(f"Array is constructed inappropriately: {shape}")
+    
+    try:
+        assert shape[0] == 1 or shape[1] == 1
+    except AssertionError:
+        raise TypeError(f"Array is not vector shaped: {shape}")
+
+
+def load_mat_file(path: PathLike) -> numpy.array:
+    """
+    Load a mat file into a DataFrame. Can only handle mat files with 1 variable inside.
+
+    Parameters
+    ----------
+    path : PathLike
+        Path to a mat file.
+    
+    Returns
+    -------
+    numpy.array
+        Array of data inside the mat file.
+
+    Raises
+    ------
+    ValueError
+        When the mat file contains more than one variable.
+    """
+    mat = scipy.io.loadmat(path)
+
+    values = []
+    for key, value in mat.items():
+        if "__" not in key:
+            values.append(value)
+    if len(values) > 1:
+        raise ValueError(f"There is more than one variable in the mat file: {path}")
+    value = values[0]
+
+    return value
 
 
 if __name__ == "__main__":
