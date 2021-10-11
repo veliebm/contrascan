51dcb49c252b61bddeba78504bab34d2ffb6ae65
veliebm <veliebm@gmail.com>
2021-10-11 08:39:48 -0400

    Remove redundant matlab reading code

--- correlate_eeg_fmri.py
+++ correlate_eeg_fmri.py
@@ -13,7 +13,9 @@ from pathlib import Path
 import pandas
 import nibabel
 import numpy
-import scipy.io
+
+# Import CSEA stuff
+from read_matlab import get_amplitudes
 
 
 def main(in_image_path: PathLike, in_eeg_path: PathLike, out_image_path: PathLike) -> None:
@@ -77,88 +79,3 @@ def convert_to_dataframe(func_image: nibabel.brikhead.AFNIImage, trim_volumes: i
     func_dataframe = pandas.DataFrame(future_dataframe)
 
     return func_dataframe
-
-
-def get_amplitudes(mat_path: PathLike) -> pandas.DataFrame:
-    """
-    Gets amplitudes from a mat file. Mat file must contain only an Nx1 vector.
-
-    Parameters
-    ----------
-    mat_path : PathLike
-        Path to a .mat file.
-    
-    Returns
-    -------
-    pandas.Series
-        Series of numbers extracted from the mat file.
-    
-    Raises
-    ------
-    TypeError
-        When the mat file contains data that isn't an Nx1 vector.
-    """
-    mat = load_mat_file(mat_path)
-    
-    _check_array_is_vector(mat)
-
-    return pandas.Series(mat.reshape(-1))
-
-
-def _check_array_is_vector(array: numpy.array) -> None:
-    """
-    Raises TypeError if the array is not 1xN or Nx1.
-
-    Parameters
-    ----------
-    array : numpy.array
-        Should be 2 dimensional and be shaped either (1,N) or (N,1)
-
-    Raises
-    ------
-    TypeError
-        Array is not 2 dimensional or is not shaped correctly.
-    """
-    shape = array.shape
-    
-    try:
-        assert len(shape) == 2
-    except AssertionError:
-        raise TypeError(f"Array is constructed inappropriately: {shape}")
-    
-    try:
-        assert shape[0] == 1 or shape[1] == 1
-    except AssertionError:
-        raise TypeError(f"Array is not vector shaped: {shape}")
-
-
-def load_mat_file(path: PathLike) -> numpy.array:
-    """
-    Load a mat file into a DataFrame. Can only handle mat files with 1 variable inside.
-
-    Parameters
-    ----------
-    path : PathLike
-        Path to a mat file.
-    
-    Returns
-    -------
-    numpy.array
-        Array of data inside the mat file.
-
-    Raises
-    ------
-    ValueError
-        When the mat file contains more than one variable.
-    """
-    mat = scipy.io.loadmat(path)
-
-    values = []
-    for key, value in mat.items():
-        if "__" not in key:
-            values.append(value)
-    if len(values) > 1:
-        raise ValueError(f"There is more than one variable in the mat file: {path}")
-    value = values[0]
-
-    return value
