c382d0734adedea3521d6fcf7f23aa9f2f732418
veliebm <veliebm@gmail.com>
2021-10-23 10:00:25 -0400

    Make parent directory of outfile

--- ttest.py
+++ ttest.py
@@ -8,13 +8,23 @@ veliebm@ufl.edu
 from typing import List
 from os import PathLike
 import subprocess
+from pathlib import Path
 
 
 def main(images: List[PathLike], prefix: PathLike) -> None:
     """
     Runs 3dttest++ on 2 sets of images.
     """
+    make_parent_directory(prefix)
+
     args = f"3dttest++ -zskip 100% -prefix {prefix}".split()
     args += ["-setA"] + [f"{path}" for path in images]
 
-    subprocess.run(args)
+    subprocess.run(args, check=True)
+
+
+def make_parent_directory(path: PathLike) -> None:
+    """
+    Create the parent directory for a path.
+    """
+    Path(path).parent.mkdir(exist_ok=True, parents=True)
