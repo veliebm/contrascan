c8acc00f2eaf9ee8ba909cffe474a85b87157332
Ben Velie <67563877+veliebm@users.noreply.github.com>
2021-11-16 08:42:07 -0500

    Make docstrings more explicit

--- align.py
+++ align.py
@@ -9,7 +9,7 @@ from typing import List
 
 def main(from_images: List[PathLike], from_template: PathLike, to_template: PathLike, to_dir: PathLike, suffix: str = "_aligned") -> None:
     """
-    Aligns 2 datasets together.
+    Using two template images, align images from one template space to another.
     """
     from_images = [Path(from_image).resolve() for from_image in from_images]
     from_template = Path(from_template).resolve()
@@ -22,7 +22,7 @@ def main(from_images: List[PathLike], from_template: PathLike, to_template: Path
 
 def align_using_templates(from_images: List[PathLike], from_template: PathLike, to_template: PathLike, cwd: PathLike, suffix: str) -> Path:
     """
-    Using two template images, align an image from one template space to another.
+    Using two template images, align images from one template space to another. Requires that input paths be full paths.
     """
     print(f"Aligning images to {to_template}")
     command = f"""
