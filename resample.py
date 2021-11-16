80bc6f64f7384e3ae6900851c65b4a4d289630ba
Ben Velie <67563877+veliebm@users.noreply.github.com>
2021-11-16 09:09:11 -0500

    Improve docstrings

--- resample.py
+++ resample.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Resample an image to another image.
+Do stuff involving resampling.
 
 Created 7/19/2021 by Benjamin Velie.
 veliebm@ufl.edu
@@ -13,6 +13,11 @@ from pathlib import Path
 def main(from_image: PathLike, to_image: PathLike, to_prefix: PathLike) -> None:
     """
     Resample an image to another image.
+
+    Args:
+        from_image (PathLike): Path to image you want to resample.
+        to_image (PathLike): Path to image you want to resample to.
+        to_prefix (PathLike): Where to write the resampled image to.
     """
     to_dir = Path(to_prefix).parent
     to_dir.mkdir(exist_ok=True, parents=True)
@@ -26,9 +31,16 @@ def main(from_image: PathLike, to_image: PathLike, to_prefix: PathLike) -> None:
     subprocess.run(command)
 
 
-def main2(from_image: PathLike, to_prefix: PathLike, dx: str, dy: str, dz: str) -> None:
+def main2(from_image: PathLike, to_prefix: PathLike, dx: float, dy: float, dz: float) -> None:
     """
     Resample an image to a new resolution.
+
+    Args:
+        from_image (PathLike): Path to image you want to resample.
+        to_prefix (PathLike): Where to write the resampled image to.
+        dx (float): x resolution.
+        dy (float): y resolution.
+        dz (float): z resolution.
     """
     to_dir = Path(to_prefix).parent
     to_dir.mkdir(exist_ok=True, parents=True)
