f79758e9a2c625103c87e1e6c107e2a6b9343249
veliebm <veliebm@gmail.com>
2021-12-17 12:14:41 -0500

    Fix typo

--- snakemake/workflow/scripts/plot_fmri.py
+++ snakemake/workflow/scripts/plot_fmri.py
@@ -32,7 +32,7 @@ def make_plot(underlay_path: PathLike, overlay_path: PathLike, save_to_path: Pat
     view = nilearn.plotting.plot_anat(underlay_path, cut_coords=coordinates, draw_cross=False, annotate=False)
     overlay_image = load_as_nifti(overlay_path)
     view.add_overlay(overlay_image, threshold=threshold, colorbar=True)
-    view.title(title, )
+    view.title(title)
 
     view.savefig(save_to_path)
     view.close()
