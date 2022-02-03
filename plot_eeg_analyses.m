9307f122f57d65c55cbcb78624f0e0d61c9ee0a0
veliebm <veliebm@gmail.com>
2022-02-03 11:42:16 -0500

    Make lines 3x thicker

--- plot_eeg_analyses.m
+++ plot_eeg_analyses.m
@@ -7,7 +7,7 @@ ssvep = eeg_analyses.ssVEP(1:end-10);
 taxis = eeg_analyses.taxis(1:end-10);
 
 %% Make plot.
-set(0, 'DefaultLineLineWidth', 4);
+set(0, 'DefaultLineLineWidth', 12);
 fig = plot(taxis, alpha);
 
 hold on;
