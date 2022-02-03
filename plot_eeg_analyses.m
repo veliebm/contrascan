eab765916d1beaa14ff7fdaf597c19b8a1833130
veliebm <veliebm@gmail.com>
2022-02-03 12:02:59 -0500

    Remove jagged bit from end of alpha line

--- plot_eeg_analyses.m
+++ plot_eeg_analyses.m
@@ -2,9 +2,9 @@
 
 %% Load variables.
 eeg_analyses = load('./data/misc/eeg_alone_analyses.mat');
-alpha = eeg_analyses.alpha(1:end-10);
-ssvep = eeg_analyses.ssVEP(1:end-10);
-taxis = eeg_analyses.taxis(1:end-10)/1000;
+alpha = eeg_analyses.alpha(1:end-30);
+ssvep = eeg_analyses.ssVEP(1:end-30);
+taxis = eeg_analyses.taxis(1:end-30)/1000;
 
 %% Make plot.
 set(0, 'DefaultLineLineWidth', 12);
